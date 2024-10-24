bdba4a6f4811baf0ed07287cb4c33e7f44bde70a
ah-ugo <ahuekweprinceugo@gmail.com>
2024-10-24 16:40:52 +0100

    consumed endpoint to list products

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -192,32 +192,37 @@ export default function FeaturedProducts() {
                     duration: 1, // Animation duration
                   },
                 }}
-                key={product.id}
+                key={product._id}
                 className="group relative px-6 flex gap-6"
               >
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                  <img
-                    alt={product.title}
-                    src={product.image_urls[0]}
-                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                  />
-                </div>
-                <div className="mt-4 flex justify-between">
-                  <div>
-                    <h3 className="text-sm text-gray-100">
-                      <a href={product?.href}>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {product.title}
-                      </a>
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-100">
-                      {product?.color}
+                <a href={`/detail/${product._id}`}>
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img
+                      alt={product.title}
+                      src={product.image_urls[0]}
+                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                  </div>
+                  <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-sm text-gray-100">
+                        <a href={product?.href}>
+                          <span
+                            aria-hidden="true"
+                            className="absolute inset-0"
+                          />
+                          {product.title}
+                        </a>
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-100">
+                        {product?.color}
+                      </p>
+                    </div>
+                    <p className="text-sm font-medium text-gray-300">
+                      {product.price}
                     </p>
                   </div>
-                  <p className="text-sm font-medium text-gray-300">
-                    {product.price}
-                  </p>
-                </div>
+                </a>
               </motion.div>
             ))}
           </Slider>
