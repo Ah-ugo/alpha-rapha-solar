4338c69d9c9d6e4f4f87206eb9f41bba1c7b6a86
ah-ugo <ahuekweprinceugo@gmail.com>
2024-11-03 07:56:54 +0100

    1:46am

--- src/Pages/ProductOverView.jsx
+++ src/Pages/ProductOverView.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ProductOverview from "../components/ProductDetail";
 import NavbarCom from "../components/NavbarCom";
+import NewNavbar from "../components/NewNavbarCom";
 
 export default function ProductOverView() {
   return (
     <div>
-      <NavbarCom
+      <NewNavbar
         className={
-          "shadow fixed z-50 w-full top-0 bg-blue-900 border-white/50 focus-within:ring-1 backdrop-blur-md transition-all duration-1000 ease-in-out"
+          "shadow fixed px-4 z-50 w-full top-0 bg-blue-900 border-white/50 focus-within:ring-1 backdrop-blur-md transition-all duration-1000 ease-in-out"
         }
       />
       <ProductOverview />
