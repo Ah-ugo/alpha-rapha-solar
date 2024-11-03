4d98873072ecb96f726dee28619edbf4cff41e0e
ah-ugo <ahuekweprinceugo@gmail.com>
2024-11-03 09:03:11 +0100

    9:03am

--- src/components/NewNavbarCom.jsx
+++ src/components/NewNavbarCom.jsx
@@ -63,7 +63,7 @@ const NewNavbar = ({ active, className }) => {
         <div
           className={`lg:ml-60 ${
             isMenuOpen ? "block" : "hidden"
-          } lg:block max-lg:before:fixed max-lg:before:bg-black max-lg:h-screen max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50`}
+          } lg:block max-lg:before:fixed max-lg:before:bg-black max-lg:h-full max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50`}
         >
           <ul
             className="lg:flex lg:gap-x-3 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-screen max-lg:shadow-md max-lg:overflow-auto"
